2ec596e01616750274a25c8c4c93a92d8aa213e4
abhinaybabugodugu <abhinaybabugodugu@gmail.com>
2015-03-29 00:55:34 +0530

    Creating Structure

--- interactive-resume.js
+++ interactive-resume.js
@@ -1,9 +1,10 @@
 $(document).ready(function(){
-$('.abc').click(
-  function()
-{
-  alert('print');
-  }
-  );
+
+$('.submit').click(function(){
+	$('.right-div .fname').html($('.left-div .fname').val());
+	$('.right-div .lname').html($('.left-div .lname').val());
+	$('.right-div .contact').html($('.left-div .contact').val());
+	$('.right-div .email').html($('.left-div .email').val());
+});
 
 });
