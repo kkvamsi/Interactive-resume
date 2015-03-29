89664459e9033f8c435b6aeaea4a4ca26a51c544
harsha1007 <harsha.kunapareddy@gmail.com>
2015-03-29 12:53:42 +0530

    JS changes to include only added elements

--- interactive-resume.js
+++ interactive-resume.js
@@ -11,13 +11,18 @@ $('.submit').click(function(){
 
 function keyPress()
     {
-            
+        if(($("#fname").val().length + $("#lname").val().length)>0){    
         var fullName = "My name is " + $("#fname").val() + " " + $("#lname").val();
 		$("#rname").text(fullName);
-
+		}
+		
+		if($("#contact").val().length>0){
 		var contactNum = "I am reachable at " + $("#contact").val();
 		$("#rcontact").text(contactNum);
+		}
 		
+		if($("#email").val().length>0){
 		var emailAdd = "Also you can send a mail to " + $("#email").val();
 		$("#remail").text(emailAdd);
+		}
     }
